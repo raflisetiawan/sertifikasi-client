a2b56a9e9d23641b7305e5490770d0538f151af8
Rafli Setiawan <dev.raflisetiawan@gmail.com>
2024-04-19 23:58:28 +0700

    fix login svg

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md-6 col-sm-0 col-xs-0" v-show="$q.screen.gt.sm">
         <div class="row justify-center items-center left-side" style="height: 100vh;">
-          <q-img src="~/assets/login.svg"></q-img>
+          <q-img src="~/assets/Login.svg"></q-img>
         </div>
       </div>
       <div class="col-md-6 col-sm-12 col-xs-12">
