e2f55603d13478099df18d6608ac18ae203d2e5b
Rafli Setiawan <dev.raflisetiawan@gmail.com>
2024-04-20 00:12:09 +0700

    Fix lightbox

--- src/components/admin/course/course-registrant/index.d.ts
+++ src/components/admin/course/course-registrant/index.d.ts
@@ -1,5 +1,5 @@
 declare module 'vue-easy-lightbox/dist/external-css/vue-easy-lightbox.esm.min.js' {
-  import VueEasyLightbox from 'vue-easy-lightbox';
+  const VueEasyLightbox = () => import('vue-easy-lightbox');
   export * from 'vue-easy-lightbox';
   export default VueEasyLightbox;
 }
